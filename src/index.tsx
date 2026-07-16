7b2bb39fe8b3afc94ea52e183e0c01d34e3a41c6
Melotwo <turoka15@gmail.com>
2026-07-16 07:38:40 +0200

    Simplify root element retrieval for ReactDOM

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <App />
 );
